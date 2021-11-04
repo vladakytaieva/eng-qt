310a7b7464733cedb5add67ca293be118ab00a2e
vladakytaieva <vlada.kitaeva2002@gmail.com>
2021-11-04 23:43:15 +0200

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react"
+import words from "./db"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [mode, setMode] = useState('eng')
+    const [word, setWord] = useState('')
+    const [correct, setCorrect] = useState('')
+    const [answer, setAnswer] = useState('')
+    const [score, setScore] = useState(0)
+    const [total, setTotal] = useState(0)
+    
+
+    useEffect(() => {
+        selectWord()
+        setTotal(0)
+        setScore(0)
+    }, [mode])
+
+    const selectWord = () => {
+        const w = words[Math.floor(Math.random() * words.length)]
+        if (mode === 'eng') {
+            setWord(w.eng)
+            setCorrect(w.qt)
+        } else {
+            setWord(w.qt)
+            setCorrect(w.eng)
+        }
+        setTotal(total + 1)
+    }
+
+    const checkAnswer = (e) => {
+        e.preventDefault()
+        if (answer === correct) {
+            console.log('well done')
+            setScore(score + 1)
+        }
+        setAnswer('')
+        selectWord()
+    }
+    
+    const changeMode = (e) => {
+        setMode(e.target.id)
+    }
+
+    return (
+        <div className="container">
+            <header>
+                <button 
+                    id="eng" 
+                    className={`btn ${mode === "eng" ? "selected" : ''}`}
+                    onClick={changeMode}
+                >Eng-Qt</button>
+                <button 
+                    id="qt" 
+                    className={`btn ${mode === "qt" ? "selected" : ''}`}
+                    onClick={changeMode}
+                >Qt-Eng</button>
+            </header>
+            <p className="message">Write {mode === 'eng' ? 'Q-transcription' : 'English word'} for the following {mode === 'eng' ? 'English word' : 'Q-transcription'}:</p>
+            <p className="word">{word}</p>
+            <form onSubmit={checkAnswer}>
+                <input type="text" value={answer} onChange={(e) => setAnswer(e.target.value)}></input><br/>
+                <button disabled={!answer} className="submit">Submit</button>
+            </form>
+            <p>Score: {score}/{total}</p>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/db.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        eng: "come",
+        qt: "kam"
+    },
+    {
+        eng: "sun",
+        qt: "san"
+    },
+    {
+        eng: "was",
+        qt: "woz"
+    },
+    {
+        eng: "fire",
+        qt: "faje"
+    },
+    {
+        eng: "cute",
+        qt: "kjuut"
+    },
+    {
+        eng: "here",
+        qt: "hie"
+    },
+]
